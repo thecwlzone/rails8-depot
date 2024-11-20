267b7e8f11b85f24ad3694266e51a24108d8bb8f
clehma203 <chris@thecwlzone.com>
2024-11-20 10:28:49 -0700

    Finish Product tests.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,17 @@
+#---
+# Excerpted from "Agile Web Development with Rails 8",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/rails8 for more book information.
+#---
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
   end
 
   test "should get index" do
@@ -17,7 +26,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, price: @product.price, title: @product.title } }
+      post products_url, params: {
+        product: {
+          description: @product.description,
+          image: file_fixture_upload("lorem.jpg", "image/jpeg"),
+          price: @product.price,
+          title: @title
+        }
+      }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +50,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, price: @product.price, title: @product.title } }
+      patch product_url(@product), params: {
+        product: {
+          description: @product.description,
+          image: file_fixture_upload("lorem.jpg", "image/jpeg"),
+          price: @product.price,
+          title: @title
+        }
+      }
+
     assert_redirected_to product_url(@product)
   end
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,89 @@
+#---
+# Excerpted from "Agile Web Development with Rails 8",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/rails8 for more book information.
+#---
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title:       "My Book Title",
+                          description: "yyy")
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"),
+                         filename: "lorem.jpg", content_type: "image/jpeg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title:       "My Book Title",
+                description: "yyy",
+                price:       1)
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"),
+                         filename: "lorem.jpg", content_type: "image/jpeg")
+  end
+
+  test "image url" do
+    product = Product.new(
+                title:       "My Book Title",
+                description: "yyy",
+                price:       1)
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"),
+      filename: "lorem.jpg", content_type: "image/jpeg")
+    assert product.valid?, "image/jpeg must be valid"
+
+    product = Product.new(
+                title:       "My Book Title",
+                description: "yyy",
+                price:       1)
+    product.image.attach(io: File.open("test/fixtures/files/logo.svg"),
+      filename: "lorem.jpg", content_type: "image/svg+xml")
+    assert_not product.valid?, "image/svg+xml must be invalid"
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:pragprog).title,
+                          description: "yyy",
+                          price:       1)
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"),
+                        filename: "lorem.jpg", content_type: "image/jpeg")
+
+    assert product.invalid?
+    assert_equal [ "has already been taken" ], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:       products(:pragprog).title,
+                          description: "yyy",
+                          price:       1)
+
+    product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"),
+                        filename: "lorem.jpg", content_type: "image/jpeg")
+
+    assert product.invalid?
+    assert_equal [ I18n.translate("errors.messages.taken") ],
+                 product.errors[:title]
+  end
 end
