8741ba3fe721183fe5a1a6bc6f0676480dadfb3c
clehma203 <chris@thecwlzone.com>
2024-11-23 16:59:09 -0700

    Add Cart and LineItem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :line_items
 
   after_commit -> { broadcast_refresh_later_to "products" }
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -12,6 +15,15 @@ class Product < ApplicationRecord
     acceptable_types = [ "image/gif", "image/jpeg", "image/png" ]
     unless acceptable_types.include?(image.content_type)
       errors.add(:image, "must be a GIF, JPG or PNG image")
+    end
+  end
+
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
     end
   end
 end
