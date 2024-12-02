a0146737526a6cc348d041fc303e7fa5ee40bcf4
clehma203 <chris@thecwlzone.com>
2024-12-01 17:02:33 -0700

    Iteration G1 - Capture Order from Cart

---
+++ db/migrate/20241201233442_add_price_to_line_item.rb
@@ -0,0 +1,9 @@
+class AddPriceToLineItem < ActiveRecord::Migration[8.0]
+  def change
+    add_column :line_items, :price, :decimal
+    LineItem.all.each do |li|
+      li.price = li.product.price
+      li.save!
+    end
+  end
+end
