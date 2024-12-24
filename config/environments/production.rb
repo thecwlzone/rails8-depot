7dcb614931d9e074c9a7525d540ba190213fbfe5
clehma203 <chris@thecwlzone.com>
2024-12-24 13:01:46 -0700

    Experimenting with Kamal...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
+  config.assets.manifest = Rails.root.join("config", "manifest.json")
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
